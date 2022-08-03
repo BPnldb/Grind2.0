735d2f099a292624d7698643d29154acc0daa563
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-08-03 14:56:41 -0700

    Time: 53 ms (73.13%), Space: 15.3 MB (15.52%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -11,29 +11,20 @@ class Solution(object):
         #Time Complexity : O(N) = 2N
         # Spaec Complexity: O(n)
         dic = {}
+        
         for i in range(len(s)):
             if s[i] not in dic:
                 dic[s[i]] = 1
             else:
                 dic[s[i]] += 1
-                
-                
-                
-                
-                
+        
         for i in range(len(t)):
             if t[i] not in dic:
                 dic[t[i]] = 1
             else:
                 if dic[t[i]] > 0:
                     dic[t[i]] -= 1
-                
-                
-                
-                
-                
-                
-                
+        
         count = 0
         for i in dic.values():
             count += i
