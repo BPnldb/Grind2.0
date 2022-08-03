b53b3cc2b1d2be18045e8985752b976543d481ad
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-08-03 14:55:06 -0700

    Time: 93 ms (19.92%), Space: 15.5 MB (9.99%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        
+        
+        #Time Complexity : O(N) = 2N
+        # Spaec Complexity: O(n)
+        dic = {}
+        for i in range(len(s)):
+            if s[i] not in dic:
+                dic[s[i]] = 1
+            else:
+                dic[s[i]] += 1
+                
+                
+                
+                
+                
+        for i in range(len(t)):
+            if t[i] not in dic:
+                dic[t[i]] = 1
+            else:
+                if dic[t[i]] > 0:
+                    dic[t[i]] -= 1
+                
+                
+                
+                
+                
+                
+                
+        count = 0
+        for i in dic.values():
+            count += i
+        return True if count == 0 else False
