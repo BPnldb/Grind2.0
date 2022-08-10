e6ac5c49ad0ff8d406a8f7cc1c8e254dcb569756
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-08-09 18:56:00 -0700

    Time: 8781 ms (5.00%), Space: 16.9 MB (31.01%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,22 +1,35 @@
 class Solution(object):
     def threeSum(self, nums):
-        res = []
+        
+        
+        
+        #time complexity : O(N ^2) for nested loops (for and while)
+        #space complexity : O(1), since it's iterating in place.
+        
+        #sort the array using some built in function
+        #this is time complexity of O(nlogn)
         nums.sort()
-
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i - 1]:
-                continue
-
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
+        
+        #initialize an empty list/array
+        result = []
+        
+        #up to len(nums) - 2 because worse cause the solution would be 
+        # [nums[i],nums[i-2],nums[i-1]]
+        
+        
+        
+        for left in range(len(nums) - 2):
+            mid = left + 1
+            right = len(nums) - 1
+            
+            while mid < right:
+                curSum = nums[left] + nums[mid] + nums[right]
+                sumList = [nums[left],nums[mid],nums[right]]
+                
+                if curSum == 0 and sumList not in result:
+                    result.append(sumList)
+                elif curSum < 0:
+                    mid += 1
                 else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
-        return res
+                    right -= 1
+        return result
