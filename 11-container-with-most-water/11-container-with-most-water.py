8b16516eeb359210c95a97cb4deb89949872f8cb
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-08-09 20:24:58 -0700

    Time: 775 ms (63.21%), Space: 24.1 MB (22.43%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -19,14 +19,16 @@ class Solution(object):
 
 
         result = 0
-        left , right = 0 , len(height) - 1
+    
+        left = 0
+        right = len(height) - 1
         
         while left < right:
-            area = (right - left) * min(height[left],height[right])
-            result = max(result, area)
+            area = abs(left-right) * min(height[left],height[right])
+            result = max(area, result)
             
-            if height[left] > height[right]:
-                right -= 1
-            else:
+            if height[left] < height[right]:
                 left += 1
+            else:
+                right -= 1
         return result
