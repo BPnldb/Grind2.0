228344540d55ad2d9ca37f89280253e3003544ae
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-08-09 18:55:22 -0700

    Time: 536 ms (90.52%), Space: 16.9 MB (31.01%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,36 +1,22 @@
 class Solution(object):
     def threeSum(self, nums):
-        
-        
-        
-        #time complexity : O(N ^2) for nested loops (for and while)
-        #space complexity : O(1), since it's iterating in place.
-        
-        #sort the array using some built in function
-        #this is time complexity of O(nlogn)
+        res = []
         nums.sort()
-        
-        #initialize an empty list/array
-        tripleList = []
-        
-        #up to len(nums) - 2 because worse cause the solution would be 
-        # [nums[i],nums[i-2],nums[i-1]]
-        
-        
-        for i in range(len(nums)-2):
-            left = i+1
-            right = len(nums) -1
-            while left < right:
-                curSum = nums[i] + nums[left] + nums[right]
-                sumList = [nums[i], nums[left] , nums[right]]
-                
-                #check if curSum is 0 and NOT IN tripleList
-                if curSum == 0 and sumList not in tripleList:
-                    tripleList.append(sumList)
-                    left += 1
-                    right -= 1
-                elif curSum < 0:
-                    left += 1
+
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
                 else:
-                    right -= 1
-        return tripleList
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        return res
