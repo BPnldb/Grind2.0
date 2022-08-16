e65808afab1342dbada73cb7ab082bfd20a8720b
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-08-16 14:40:27 -0700

    Time: 125 ms (69.74%), Space: 13.4 MB (99.47%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -9,10 +9,12 @@ class Solution(object):
         res = 0 
         l = 0
         r = 0
+        maxFreq = 0
         while r < len(s):
             count[s[r]] = 1 + count.get(s[r],0)
+            maxFreq = max(maxFreq, count[s[r]])
             
-            if (r-l+1) - max(count.values()) > k:
+            if (r-l+1) - maxFreq > k:
                 count[s[l]] -= 1
                 l += 1
             
