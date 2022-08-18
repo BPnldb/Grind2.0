ed2fdd8dd7677671dd44a3a04f83c2c0e7360180
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-08-17 21:20:30 -0700

    Time: 382 ms (14.97%), Space: 14.5 MB (94.78%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -5,15 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        left, right = 0, len(nums) -1
+        left, right = 0, len(nums) - 1
         
         while left <= right:
-            mid = (left + right) // 2
-            
+            mid = (left + right) //2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
-                left = mid +1
+                left += 1
             else:
-                right = mid - 1
+                right -= 1
         return -1
+        
+        
