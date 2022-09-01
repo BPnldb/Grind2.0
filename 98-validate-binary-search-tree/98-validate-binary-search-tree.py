c93788dff1c8a3b36418c5ada9cab792eb2077d7
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-09-01 13:59:05 -0700

    Time: 53 ms (45.86%), Space: 18 MB (66.12%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -10,12 +10,13 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-        def dfs(node, left, right):
+        
+        
+        def dfs(node,left,right):
             if not node:
                 return True
-            if not (node.val > left and node.val < right):
+            if not (node.val < right and node.val > left):
                 return False
-            
             return dfs(node.left, left, node.val) and dfs(node.right, node.val, right)
         
-        return dfs(root, float("-inf"),float("inf"))
+        return dfs(root, float("-inf"), float("inf"))
