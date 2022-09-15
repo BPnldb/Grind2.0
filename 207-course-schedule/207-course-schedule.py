dddfc0ae789eb03b4e3e75ad6e72a6f3c19605e0
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-09-14 17:41:54 -0700

    Time: 104 ms (92.84%), Space: 15.6 MB (70.66%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,30 +1,57 @@
 class Solution:
-    def canFinish(self, numCourses, pre):
-        adj_list = {i:[] for i in range(numCourses)}
+    def canFinish(self, numCourses, prerequisites):
         
-        for u,v in pre:
-            adj_list[u].append(v)
+        #DFS
+#         adj_list = {i:[] for i in range(numCourses)}
+#         for u,v in pre:
+#             adj_list[u].append(v)
         
-        visited = set()
+#         visited = set()
         
-        def dfs(course):
-            if course in visited:
-                return False
+#         def dfs(course):
+#             if course not in visited:
+#                 if adj_list[course] == []:
+#                     return True
+
+#                 visited.add(course)
+
+#                 for p in adj_list[course]:
+#                     result = dfs(p)
+#                     if not result:
+#                         return False
+#                 visited.remove(course)
+#                 adj_list[course] = []
+#                 return True
+
+#                 for course in range(numCourses):
+#                     if not dfs(course):
+#                         return False
+#                 return True
+#             return False
+        #BFS
+    
+        visited = [0] * numCourses
+        graph = defaultdict(list)
+        for courses in prerequisites:
+            graph[courses[1]].append(courses[0])
+            visited[courses[0]] += 1
             
-            if adj_list[course] == []:
-                return True
+        # find a node with no incoming edge and add it to queue.
+        queue = deque()
+        for i in range(numCourses):
+            if visited[i] == 0:
+                queue.append(i)
+                
+        # topological sort algorithm - bfs approach.
+        total_courses = 0
+        while queue:
+            course = queue.popleft()
+            total_courses += 1
+            for prerequisite in graph[course]:
+                visited[prerequisite] -= 1
+                if visited[prerequisite] == 0:
+                    queue.append(prerequisite)
+                    
+        return total_courses == numCourses
             
-            visited.add(course)
-            
-            for p in adj_list[course]:
-                result = dfs(p)
-                if not result:
-                    return False
-            visited.remove(course)
-            adj_list[course] = []
-            return True
-            
-        for course in range(numCourses):
-            if not dfs(course):
-                return False
-        return True
+        
