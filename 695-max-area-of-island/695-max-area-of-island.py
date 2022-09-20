335c02830836a8b06a986a1de0d70b4022d25431
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-09-19 21:15:40 -0700

    Time: 594 ms (5.00%), Space: 16.9 MB (12.10%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        # have a visit hash set to mark positions we already visited
+        #T: O(M * N)
+        
+        ROWS , COLS =  len(grid), len(grid[0])
+        visit = set()
+        
+        def dfs(r, c): #position we pass in
+            #base case out of bounds
+            if (r not in range(ROWS) or c not in range(COLS) or grid[r][c] == 0 or (r,c) in visit):
+                return 0
+            
+            visit.add((r,c))
+            return (1 + dfs(r + 1,c)+
+                        dfs(r - 1, c)+
+                        dfs(r, c + 1)+
+                        dfs(r, c - 1))
+        
+        area = 0
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                area = max(area, dfs(r,c))
+        return area
