5c57295782af8b11d7b2435681c0e76c788c842a
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-09-21 00:01:24 -0700

    Time: 40 ms (91.96%), Space: 14.1 MB (28.26%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.py
+++ 684-redundant-connection/684-redundant-connection.py
@@ -4,23 +4,32 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: List[int]
         """
-        parent = [-1] * (len(edges) + 1)
+        par = [i for i in range(len(edges) + 1)]
+        rank = [1] * (len(edges) + 1)
         
         def find(node):
-            while parent[node] > 0:
-                node = parent[node]
-            return node
+            p = par[node]
+            while p != par[p]:
+                #par[p] = par[par[p]]
+                p = par[p]
+            return p
         
+        #return false if can't complete (already merged)
         def union(node1, node2):
-            one, two = find(node1), find(node2)
+            p1, p2 = find(node1), find(node2)
             
-            if one == two:
+            if p1==p2:
                 return False
             
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+        
             else:
-                parent[one] = two
-                return True
-            
-        for u, v in edges:
-            if not union(u,v):
-                return [u,v]
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            return True
+    
+        for node1, node2 in edges:
+            if not union(node1, node2):
+                return [node1, node2]
