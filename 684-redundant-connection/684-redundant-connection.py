ce8512b47dd5ca6da84430fd10486b8ed2ffcc9b
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-09-20 23:58:30 -0700

    Time: 69 ms (45.43%), Space: 13.8 MB (81.30%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.py
+++ 684-redundant-connection/684-redundant-connection.py
@@ -6,28 +6,30 @@ class Solution(object):
         """
         par = [i for i in range(len(edges) + 1)]
         rank = [1] * (len(edges) + 1)
-
-        def find(n):
-            p = par[n]
+        
+        def find(node):
+            p = par[node]
             while p != par[p]:
                 par[p] = par[par[p]]
                 p = par[p]
             return p
-
-        # return False if already unioned
-        def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
-
-            if p1 == p2:
+        
+        #return false if can't complete (already merged)
+        def union(node1, node2):
+            p1, p2 = find(node1), find(node2)
+            
+            if p1==p2:
                 return False
+            
             if rank[p1] > rank[p2]:
                 par[p2] = p1
                 rank[p1] += rank[p2]
+        
             else:
                 par[p1] = p2
                 rank[p2] += rank[p1]
             return True
-
-        for n1, n2 in edges:
-            if not union(n1, n2):
-                return [n1, n2]
+    
+        for node1, node2 in edges:
+            if not union(node1, node2):
+                return [node1, node2]
