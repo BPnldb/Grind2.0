4545c871ce65fa50413a151fad9a9e7a4535fef2
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-09-27 00:42:49 -0700

    Time: 74 ms (5.58%), Space: 14 MB (39.24%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -1,7 +1,10 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         print(s)
-        newS = s.strip().split(' ')
-        newS = list(newS)
-        print(newS)
-        return len(newS[-1])
+        newS = s.strip()
+        newS = newS.split(' ')
+        res = []
+        for i in newS:
+            if i is not ' ':
+                res.append(i)
+        return len(res[-1])
