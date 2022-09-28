ce7694f63cbfe051913b1d2237d9357e9b0f4f0c
Brandon <78186392+BPnldb@users.noreply.github.com>
2022-09-27 20:39:56 -0700

    Time: 59 ms (5.23%), Space: 15.3 MB (76.42%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -5,36 +5,15 @@
 #         self.next = next
 class Solution(object):
     def reverseList(self, head):
-       
-    
         """
-        -break down into sub probs
-        -instead of entire LL, reverse the remainder
-         
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev = None
         
-
-        
-        #Time Complexity : O(N)
-        #Space Complexity : O(1)
-    
-        start = None
         while head:
             temp = head.next
-            head.next  = start
-            start  = head
+            head.next = prev
+            prev = head
             head = temp
-        return start
-        
-        """
-        
-        #recursive
-        # T:O(N)
-        # S:O(N) creates new memory stack every time it calls itself.
-        
-        #base case
-        if not head or not head.next:
-            return head
-        current = self.reverseList(head.next)
-        head.next.next = head
-        head.next = None
-        return current
+        return prev
